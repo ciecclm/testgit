b5f29c148dd831d36af9403df41c851029cc70ed
ciecclm <clm@imnu.edu.cn>
2016-03-21 21:28:06 +0800

    Initial Commit

---
+++ testgit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testgit
+//
+//  Created by admin on 16/3/21.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
