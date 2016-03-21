87771590bb8fc72f3083cd9c8ad0873b98378b13
ciecclm <clm@imnu.edu.cn>
2016-03-21 21:38:56 +0800

    Revert "22222222222222"
    This reverts commit 32744a9cdb8640e155d6a6fd24978fb0fce0d874.
    
    Conflicts:
    	testgit/main.cpp

--- testgit/main.cpp
+++ testgit/main.cpp
@@ -11,8 +11,11 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+<<<<<<< HEAD
     std::cout << "22222222222222\n";
     std::cout << "333333333333333\n";
 
+=======
+>>>>>>> parent of 32744a9... 22222222222222
     return 0;
 }
